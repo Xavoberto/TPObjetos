50f29d7e7d3eef9e6a31fac84667db57467f67c6
Tomas <tomasaotds@gmail.com>
2021-07-05 16:23:23 -0300

    Pantallas Consultas

---
+++ TP Objetos/TP/src/vistas/FrmConsultas1.java
@@ -0,0 +1,12 @@
+package vistas;
+
+import javax.swing.*;
+
+public class FrmConsultas1 {
+    private JPanel panel1;
+    private JComboBox comboBox1;
+    private JButton buttonConsultaCuentaCorriente;
+    private JButton buttonEmitirOrdenesDePago;
+    private JButton ButtonDeudaPorProveedor;
+    private JButton buttonImpuestosRetenidos;
+}
---
+++ TP Objetos/TP/src/vistas/FrmConsultas2.java
@@ -0,0 +1,17 @@
+package vistas;
+
+import javax.swing.*;
+
+public class FrmConsultas2 {
+    private JPanel panel1;
+    private JComboBox comboCuitProveedorBuscarFactura;
+    private JTextField fechaBuscarFacturaField;
+    private JButton buttonBuscarFacturas;
+    private JComboBox comboRubroConsultaDePrecios;
+    private JTextField productoServicioConsultaDePreciosField;
+    private JButton buttonConsultaDePrecios;
+    private JComboBox comboCuitProveedorConsultaLibroIva;
+    private JComboBox comboDocumentoConsultaLibroIva;
+    private JButton buttonBuscarDocumentosConsultaLibroIva;
+    private JButton buttonConsultaLibroIva;
+}
